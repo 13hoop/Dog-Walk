5bad86baf2600caf6017744a65d8f6d6ef39e21e
YongRen <zhanxin13@gmail.com>
2015-09-18 14:53:47 +0800

    1 model data; 2 add managed object subclass Sum：Xcode创建.xcdatamodeld文件＋设置关联

---
+++ Dog.swift
@@ -0,0 +1,16 @@
+//
+//  Dog.swift
+//  Dog Walk
+//
+//  Created by YongRen on 15/9/18.
+//  Copyright © 2015年 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Dog: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
